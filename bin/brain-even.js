3a919bc45aa6c9333eeefb26dd496c3075703660
Vladislav <justdodevelopment@gmail.com>
2023-06-09 16:32:58 +0300

    fixed errors and warnigs from Eslint

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,3 @@
-import gameEven from "../src/even.js";
+import gameEven from '../src/even.js';
 
 gameEven();
--- src/even.js
+++ src/even.js
@@ -2,17 +2,19 @@ import readlineSync from 'readline-sync';
 import greeting from './cli.js';
 
 export default function gameEven() {
-  let name = greeting();
-  console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
-  const count = 3
+  const name = greeting();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const count = 3;
   for (let i = 0; i < count; i += 1) {
-    const getRandomInt = function(max) {
-      return Math.floor(Math.random() * max);
-    };
+    const getRandomInt = (max) => Math.floor(Math.random() * max);
+
     const currentRandom = getRandomInt(100);
+
     console.log(`Question: ${currentRandom}`);
-    let answerFromUser = readlineSync.question(`Your answer: `)
-    let rigthAnswer = (currentRandom % 2 === 0) ? 'yes' : 'no';
+
+    const answerFromUser = readlineSync.question('Your answer: ');
+
+    const rigthAnswer = (currentRandom % 2 === 0) ? 'yes' : 'no';
     if (rigthAnswer === answerFromUser) {
       console.log('Correct!');
     } else {
