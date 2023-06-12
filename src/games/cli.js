6166484b8fe259e41712b3c8a83cdbd406c3594e
Vladislav <justdodevelopment@gmail.com>
2023-06-13 00:50:16 +0300

    fix

--- src/games/cli.js
+++ src/games/cli.js
@@ -4,5 +4,4 @@ export default function greeting() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
-  return name;
 }
